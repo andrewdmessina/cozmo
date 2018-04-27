02d10ce827befc211e22c0195127e8d861808e35
Matt Bernardini <matt.bernardini@gmail.com>
2018-04-27 11:39:53 -0500

    updates from tensorflow branch

--- scripts/take_pictures.py
+++ scripts/take_pictures.py
@@ -3,12 +3,11 @@ from cozmo.util import degrees
 import time
 import sys
 import os
-
+imageNumber = 200
 def on_new_camera_image(evt, **kwargs):
-    global rtValue
+    print("here", directory)
     pilImage = kwargs['image'].raw_image
-    pilImage.save("images/" + directory + "/" + directory + "-%d.jpg" % kwargs['image'].image_number, "JPEG")
-    rtValue = "images/" + directory + "/" + directory + "-%d.jpg" % kwargs['image'].image_number
+    pilImage.save("images/" + directory + "/" + directory + "-11%d.jpg" % kwargs['image'].image_number, "JPEG")
 
 
 def cozmo_program(robot: cozmo.robot.Robot):
@@ -24,23 +23,27 @@ def cozmo_program(robot: cozmo.robot.Robot):
     print("Done: Taking images")
 
 def tf_cozmo_program(robot: cozmo.robot.Robot):
-    #robot.set_head_angle(degrees(10.0)).wait_for_completed()
-    #robot.set_lift_height(0.0).wait_for_completed()
+    robot.set_head_angle(degrees(10.0)).wait_for_completed()
+    robot.set_lift_height(0.0).wait_for_completed()
     global directory
     if not os.path.exists('images'):
         os.makedirs('images')
     if not os.path.exists('images/' + directory):
         os.makedirs('images/' + directory)
     robot.add_event_handler(cozmo.world.EvtNewCameraImage, on_new_camera_image)
+    time.sleep(.1)
 
 def take_pictures():
-    global directory
-    directory = input("Please enter object name:\n")
-    print("Cozmo will take pictures for 10 seconds, please rotate him around the object in order to get the best predictions")
-    input("Press enter to continue when you are ready")
-    cozmo.run_program(cozmo_program, use_viewer=True, force_viewer_on_top=True)
+    while (True):
+        global directory
+        directory = input("Please enter object name:\n")
+        if (directory == "done"):
+            return
+        print("Cozmo will take pictures for 10 seconds, please rotate him around the object in order to get the best predictions")
+        input("Press enter to continue when you are ready")
+        cozmo.run_program(cozmo_program, use_viewer=True, force_viewer_on_top=True)
 
 def tf_picture_taker():
     global directory, rtValue
-    cozmo.run_program(tf_cozmo_program, use_viewer=False, force_viewer_on_top=False)
-    return rtValue
+    directory = "label"
+    cozmo.run_program(tf_cozmo_program, use_viewer=True, force_viewer_on_top=False)
