6682cdef561a8b70ba7b03413987325e6e458afd
Michael Lee <mlcmc@mail.umsl.edu>
2018-05-03 08:41:42 -0500

    Cozmo is scoring player decisions and the game exits at the score limit. TODO: 	Add cute reactions. 	Let Cozmo hit the block as well. 	Retrain the network.

--- cozmo_game/main.py
+++ cozmo_game/main.py
@@ -4,6 +4,8 @@ from time import sleep
 import asyncio
 from cozmo.lights import Light, Color
 from label_image import get_opinon
+POINTS_NEEDED_TO_WIN = 3
+
 orange = (Color(name="orange", int_color=0xffab41ff))
 orange_light = Light(on_color=orange, off_color=orange)  # Internal bug, the two must match
 yellow = (Color(name="yellow", int_color=0xfdff00ff))
@@ -12,17 +14,23 @@ purple = (Color(name="purple", int_color=0xb000ffff))
 purple_light = Light(on_color=purple, off_color=purple)  # Internal bug, the two must match
 
 smells = {
-    "Coffee": cozmo.lights.red_light, # changed to red
-    "Baby Powder": cozmo.lights.blue_light,
-    "Citrus": yellow_light,
-    "Vanilla": cozmo.lights.white_light, # changed to white
-    "Mint": cozmo.lights.green_light
+    "coffee": cozmo.lights.red_light, # changed to red
+    "baby powder": cozmo.lights.blue_light,
+    "citrus": yellow_light,
+    "vanilla": cozmo.lights.white_light, # changed to white
+    "mint": cozmo.lights.green_light
 }
 
 players = {  # Decided at runtime, but it may look something like:
-    # "Cozmo": LightCube3
-    # "PlayerOne": LightCube1
-    # "PlayerTwo": LightCube2
+    # "cozmo": LightCube3
+    # "player1": LightCube1
+    # "player2": LightCube2
+}
+
+score = {
+    'cozmo': 0,
+    'player1': 0,
+    'player2': 0
 }
 
 
@@ -30,6 +38,11 @@ class GameCube(cozmo.objects.LightCube):
     def __init__(self, *a, **kw):
         super().__init__(*a, **kw)
         self._cycle = None
+        self.color = None
+
+    def set_and_remember_lights(self, light_color: cozmo.lights):
+        self.color = light_color
+        self.set_lights(light_color)
 
     def start_light_cycle(self):
         if self._cycle:
@@ -38,9 +51,7 @@ class GameCube(cozmo.objects.LightCube):
         async def _cycle():
             while True:
                 for color in smells.values():
-                    players["cozmo"].set_lights(color)
-                    players["one"].set_lights(color)
-                    players["two"].set_lights(color)
+                    self.set_and_remember_lights(color)
                     await asyncio.sleep(0.5, loop=self._loop)
         self._cycle = asyncio.ensure_future(_cycle(), loop=self._loop)
 
@@ -52,29 +63,23 @@ class GameCube(cozmo.objects.LightCube):
 
 cozmo.world.World.light_cube_factory = GameCube
 # Cozmo queues responses which aren't being handled, but have been triggered.
-# At the start of the game we could have Cozmo go and grab a random cube and make that one his.
 # https://github.com/anki/cozmo-python-sdk/blob/master/examples/tutorials/05_async_python/01_cube_blinker_sync.py#L77
 
 
-def get_opinion() -> {}:
-    return choice(list(smells.values()))
+def grade_player_decision(tapped_cube: GameCube, cozmos_opinion: cozmo.lights) -> bool:
+    player_who_tapped = [k for k,v in players.items() if v == tapped_cube][0]
+    if tapped_cube.color == cozmos_opinion:  # If the color of that thing is correct, win
+        score[player_who_tapped] += 1
+        return True
+    else:  # bonk
+        for player_name in players.keys():
+            if player_name != player_who_tapped:
+                score[player_name] += 1
+        return False
 
 
 async def cozmo_program(robot: cozmo.robot.Robot):
     robot.set_head_light(enable=True)
-    def player_choice(evt, **kw):
-        print("inside event handler")
-        global players
-        players["cozmo"].stop_light_cycle()
-        players["one"].stop_light_cycle()
-        players["two"].stop_light_cycle()
-        players["cozmo"].set_lights_off()
-        players["one"].set_lights_off()
-        players["two"].set_lights_off()
-        winner = [k for k,v in players.items() if v == evt.obj][0]
-        print(winner)
-        return evt.obj
-
     robot.world.auto_disconnect_from_cubes_at_end(False)  # Takes a while to connect
     await robot.world.connect_to_cubes()  # Will be skipped if Cozmo is connected already.
     look_around = robot.start_behavior(cozmo.behavior.BehaviorTypes.LookAroundInPlace)
@@ -82,33 +87,25 @@ async def cozmo_program(robot: cozmo.robot.Robot):
     try:
         players["cozmo"] = await robot.world.wait_for_observed_light_cube(timeout=60)
     except asyncio.TimeoutError:
-        #robot.say_text("SOILED IT")
         return
     finally:
         look_around.stop()
-        #robot.say_text("I'M READY").wait_for_completed()
 
     #  Set the players with their cubes.
-    players["one"] = choice([x for x in list(robot.world.light_cubes.values()) if x not in list(players.values())])
-    players["two"] = choice([x for x in list(robot.world.light_cubes.values()) if x not in list(players.values())])
-    score = {
-        'cozmo': 0,
-        'player1': 0,
-        'player2': 0
-    }
-    # def cube_tapped2(evt, **kw):
-    #     print("Interest\ning")
+    players["player1"] = choice([x for x in list(robot.world.light_cubes.values()) if x not in list(players.values())])
+    players["player2"] = choice([x for x in list(robot.world.light_cubes.values()) if x not in list(players.values())])
+
     await robot.set_lift_height(1).wait_for_completed()
     await robot.go_to_object(players["cozmo"], distance_from_object=cozmo.util.Distance(35)).wait_for_completed()
 
-
-    for x in range(1):
+    while max(score.values()) < POINTS_NEEDED_TO_WIN:
         await robot.drive_straight(cozmo.util.distance_mm(-10), cozmo.util.speed_mmps(50)).wait_for_completed()
         print("Waiting for cozmo to smell")
         await robot.turn_in_place(cozmo.util.degrees(90)).wait_for_completed()
         await robot.set_head_angle(cozmo.util.degrees(5)).wait_for_completed()
         have_valid_picture = False
         while not have_valid_picture:
+            # Sniff sound
             opinons = [graph[1][graph[0].index(1)] for graph in await get_opinon(robot)]
             if len(set(opinons)) == 2:
                 have_valid_picture = True
@@ -120,29 +117,36 @@ async def cozmo_program(robot: cozmo.robot.Robot):
         print("Cozmo has an opinion")
 
         print(opinons)
+        cozmos_guess = [x for x in set(opinons) if x != "junk"][0]
+        print(f"Cozmo thinks the smell is: " + cozmos_guess)
         players["cozmo"].start_light_cycle()
-        players["one"].start_light_cycle()
-        players["two"].start_light_cycle()
+        players["player1"].start_light_cycle()
+        players["player2"].start_light_cycle()
 
         try:
-            winner = await robot.wait_for(cozmo.objects.EvtObjectTapped)
-            print(winner)
-            print(players.keys()[players.values()[winner]])
+            first_cube_tap = await robot.wait_for(cozmo.objects.EvtObjectTapped, timeout=30)
+            first_person_to_tap = [k for k,v in players.items() if v == first_cube_tap.obj][0]
         except asyncio.TimeoutError:
+            # Probably restart or teardown the game
             continue
         finally:
-            print(winner.obj)
+            first_cube_tap.obj.stop_light_cycle()
+            print(f"Player: " + first_person_to_tap + f" tapped first.")
+            for cube in set(players.values()) - set([first_cube_tap.obj]):
+                cube.stop_light_cycle()
+                cube.set_lights_off()
+            player_was_correct = grade_player_decision(first_cube_tap.obj, smells[cozmos_guess])
+            if player_was_correct:
+                robot.say_text("Agree")
+            else:
+                robot.say_text("Disagree")
 
-        # try:
-        #     print("Waiting for player opinion")
-        #     #players["one"].wait_for_tap(timeout=20)
-        #     #players["two"].wait_for_tap(timeout=20)
-        #     #players["cozmo"].wait_for_tap(timeout=20)
-        #     print("Cube tapped")
-        # except asyncio.TimeoutError:
-        #     print("Players must have left, restart?")
-        # finally:
-
+        print("Pausing execution until an object is moved.")
+        await robot.wait_for(cozmo.objects.EvtObjectMovingStarted,  timeout=30)
+        #teardown for next test iteration
+        for cube in players.values():
+            cube.stop_light_cycle()
+            cube.set_and_remember_lights(cozmo.lights.off_light)
 
 cozmo.robot.Robot.drive_off_charger_on_connect = False
 cozmo.run_program(cozmo_program, use_viewer=True, force_viewer_on_top=True)
